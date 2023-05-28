c6df9a3bde1473c9a87377489178e2e02fa8f60e
Kawtar Choubari <kawtar.choubari@zenpark.com>
2023-05-28 18:04:52 +0100

    Featured Work Section in Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
+import ProjectCard from "@/components/project-card";
+import { CodingProjects } from "@/content/work";
 export function Hero() {
   return (
-    <div className="md:flex md:flew-row justify-between py-28 md:px-40">
+    <div className="md:flex md:flew-row justify-between py-28 mx-4 md:px-20">
       <div className="basis-7/12 pr-16 bg-dark">
         <h1 className="text-5xl pb-10 font-serif font-bold leading-tight">
           Coding & Storytelling:
@@ -37,10 +40,40 @@ export function Hero() {
     </div>
   );
 }
+
+export function FeaturedProjects() {
+  return (
+    <div className="mx-4 my-6 md:px-20">
+      <div className="flex flex-col mb-5">
+        <h1 className="font-serif text-4xl font-bold mb-2">
+          Featured Work
+          <span className="text-4xl leading-3 text-accent">.</span>
+        </h1>
+        <p>
+          Here are some of the projects I've worked on, utilizing a variety of
+          technologies and frameworks.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-4">
+        {CodingProjects.slice(0, 3).map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
+      </div>
+      <div className="text-center">
+        <Link href="/work">
+          <p className="inline-block px-6 py-3 rounded-full bg-gradient-to-r from-accent to-vibrant-blue text-white text-sm font-semibold">
+            View All Projects
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
+}
 export default function Home() {
   return (
     <>
       <Hero />
+      <FeaturedProjects />
     </>
   );
 }
