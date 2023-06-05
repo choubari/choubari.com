bbb5134571a9ea043a21e2182fe28ba5b1eca01c
Kawtar Choubari <kawtar.choubari@zenpark.com>
2023-06-05 20:23:03 +0100

    Draft 404 Page

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+import Button from "@/components/ui/button";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col justify-center align-middle items-center my-20">
+      <h1 className="font-serif text-5xl font-bold mb-2">
+        404
+        <span className="text-6xl leading-3 text-accent">.</span> Not Found
+        <span className="leading-3 text-accent">!</span>
+      </h1>
+      <p className="mb-5">Could not find this page!</p>
+      <Button route={"/"} label={"HOME PAGE"} />
+    </div>
+  );
+}
