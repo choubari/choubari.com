fbf4c00e1ef947abf562b863800191e0cbd5b6d0
Kawtar Choubari <kawtar.choubari@zenpark.com>
2023-09-05 23:27:36 +0200

    404 page little update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,12 +4,14 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex flex-col justify-center align-middle items-center my-20">
-      <h1 className="font-serif text-5xl font-bold mb-2">
+      <h1 className="font-serif text-5xl font-bold my-20">
         404
         <span className="text-6xl leading-3 text-accent">.</span> Not Found
         <span className="leading-3 text-accent">!</span>
       </h1>
-      <p className="mb-5">Could not find this page!</p>
+      <p className="mb-5">
+        Could not find this page! Better to be back Home ;)
+      </p>
       <Button route={"/"} label={"HOME PAGE"} />
     </div>
   );
