d46d073d9185b4cfd47eb3f0ea9ed87230151263
Kawtar Choubari <kawtar.choubari@zenpark.com>
2023-09-08 15:31:11 +0200

    Config Site

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types";
+
+export const siteConfig: SiteConfig = {
+  name: "Kawtar Choubari",
+  description:
+    "choubari.com is the personal website of Kawtar CHOUBARI, a Software Engineer and Content Creator. Built with Next.js 13.",
+  url: "https://choubari.com",
+  ogImage: "https://choubari.com/og.jpg",
+  links: {
+    twitter: "https://twitter.com/choubari_",
+    github: "https://github.com/choubari",
+  },
+};
