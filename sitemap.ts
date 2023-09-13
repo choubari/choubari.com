44b5358b5d0e12f28d2693ce4bbc6fb0c9648512
Kawtar Choubari <kawtar.choubari@zenpark.com>
2023-09-13 22:47:32 +0200

    Add Sitemap

---
+++ sitemap.ts
@@ -0,0 +1,30 @@
+import { allPosts } from "contentlayer/generated";
+
+const home = process.env.NEXT_PUBLIC_APP_URL;
+
+export default async function sitemap() {
+  const dynamicRoutes = allPosts
+    .filter((post) => post.published)
+    .map((post) => ({
+      url: `${home}/blog/${post.slugAsParams}`,
+      lastModified: new Date().toISOString(),
+    }));
+
+  const staticRoutes = [
+    "",
+    "/about",
+    "/work",
+    "/talks",
+    "/blog",
+    "/contact",
+    "/testimonials",
+    "/oss",
+    "/creator",
+    "/newsletter",
+  ].map((route) => ({
+    url: `${home}/${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...dynamicRoutes, ...staticRoutes];
+}
