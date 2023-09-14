41ec0f446b8689beb577fe5a5a64d6ff9aaad507
Kawtar Choubari <kawtar.choubari@zenpark.com>
2023-09-14 18:18:34 +0200

    Change default theme to dark

--- lib/provider.tsx
+++ lib/provider.tsx
@@ -5,5 +5,9 @@
 import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      {children}
+    </ThemeProvider>
+  );
 }
