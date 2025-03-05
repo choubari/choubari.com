90cd72eb11651f2d02e88171e3ae694d79cb4401
Kawtar Choubari <choubari.kawtar@gmail.com>
2025-03-06 00:34:38 +0100

    add new testimonials

--- content/testimonials.ts
+++ content/testimonials.ts
@@ -2,6 +2,84 @@ import { Feedback, FEEDBACK_CATEGORY } from "@/types";
 
 export const Testimonials: Feedback[] = [
   {
+    name: "Aditya Sharan",
+    message:
+      "Great session 👍 (with @OReillyMedia) I attended and found the insights on how React Server Components optimize rendering and data fetching fascinating !!💯💯",
+    position: "Software Engineer",
+    company: "Google",
+    photo: "aditya.jpg",
+    source: "Twitter",
+    link: "https://x.com/adityasharan01/status/1829021444136702276",
+    date: "August 2024",
+    category: [FEEDBACK_CATEGORY.WORK, FEEDBACK_CATEGORY.COMMUNITY],
+  },
+  {
+    name: "Ibrahim Mestadi",
+    message:
+      "Kawtar, the former Chair of the IEEE ENSIAS Student Branch in 2020, is a standout collaborator. We achieved success together on projects like building the branch's website and excelling in the Gitex Dubai IoT & AI challenge under her guidance.\n As a university peer, she's known for her kindness, strong problem-solving skills, and creative flair in front-end development and UI/UX design.\n I confidently endorse her for any future endeavors.",
+    position: "Software Engineer",
+    company: "Inyad",
+    photo: "ibrahim.jpg",
+    date: "September 2023",
+    category: [
+      FEEDBACK_CATEGORY.LEADERSHIP,
+      FEEDBACK_CATEGORY.COMMUNITY,
+      FEEDBACK_CATEGORY.DESIGN,
+    ],
+  },
+  {
+    name: "Mohamed Essaaidi",
+    message:
+      "I was very pleased to work with Kawtar during her term as the chair of the IEEE ENSIAS Student Branch as the Counsellor of this IEEE entity. This term witnessed amazing achievements thanks to Kawtar's leadership and wonderful interpersonal and managerial skills.\n I sincerely believe that Kawtar is an inspiring leader to engineering students and to girls and women in Morocco and worldwide.",
+    position: "Professor | Inventor | IEEE SIGHT Chair | Director",
+    company: "EMSI",
+    photo: "ProfEssaaidi.jpg",
+    source: "Linkedin",
+    link: "https://www.linkedin.com/in/choubari/details/recommendations/",
+    date: "April 2022",
+    category: [FEEDBACK_CATEGORY.LEADERSHIP, FEEDBACK_CATEGORY.COMMUNITY],
+  },
+  {
+    name: "Pierre Burgy",
+    message: "Your talk was great, thank you!",
+    position: "Co-founder & CEO",
+    company: "Strapi",
+    photo: "pierre.jpg",
+    source: "Twitter",
+    link: "https://x.com/pierre_burgy/status/1795521979220627527",
+    date: "May 2024",
+    category: [FEEDBACK_CATEGORY.WORK, FEEDBACK_CATEGORY.COMMUNITY],
+  },
+  {
+    name: "Aïcha GUEDIRA",
+    message:
+      "I’ve been following @choubari_ on LinkedIn since she was studying and was already impressed (and proud to say the least)! 🥰 She’s a powerful example of breaking down prejudices about the impact of women in tech! 🔥🇲🇦",
+    position: "Head of People",
+    company: "Theodo Morocco",
+    photo: "aicha.jpg",
+    source: "Twitter",
+    link: "https://x.com/AichaGuedira/status/1795030605098320241",
+    date: "May 2024",
+    category: [
+      FEEDBACK_CATEGORY.LEADERSHIP,
+      FEEDBACK_CATEGORY.GROWTH,
+      FEEDBACK_CATEGORY.COMMUNITY,
+    ],
+  },
+
+  {
+    name: "Amélie Benoit",
+    message:
+      "New Sketchnote of #ReactParis on @choubari_'s talk about Partial pre-rendering: Hype or Innovation ? Yet another great and insightful talk 🌟",
+    position: "Tech Lead Manager",
+    company: "Busbud",
+    photo: "amelie.jpg",
+    source: "Twitter",
+    link: "https://x.com/AmelieBenoit33/status/1777597267064541535",
+    date: "April 2024",
+    category: [FEEDBACK_CATEGORY.WORK, FEEDBACK_CATEGORY.COMMUNITY],
+  },
+  {
     name: "François Souterelle",
     message:
       "Kawtar has a great appetite for learning and she understands fast! During her internship at Zenpark, she successfully contributed to the development of our mobile apps.\n Kawtar also shows a very positive attitude and is genuinely happy to share her discoveries with the team.",
@@ -33,33 +111,7 @@ export const Testimonials: Feedback[] = [
       FEEDBACK_CATEGORY.LEADERSHIP,
     ],
   },
-  {
-    name: "Mohamed Essaaidi",
-    message:
-      "I was very pleased to work with Kawtar during her term as the chair of the IEEE ENSIAS Student Branch as the Counsellor of this IEEE entity. This term witnessed amazing achievements thanks to Kawtar's leadership and wonderful interpersonal and managerial skills.\n I sincerely believe that Kawtar is an inspiring leader to engineering students and to girls and women in Morocco and worldwide.",
-    position: "Professor | Inventor | IEEE SIGHT Chair | Director",
-    company: "EMSI",
-    photo: "ProfEssaaidi.jpg",
-    source: "Linkedin",
-    link: "https://www.linkedin.com/in/choubari/details/recommendations/",
-    date: "April 2022",
-    category: [FEEDBACK_CATEGORY.LEADERSHIP, FEEDBACK_CATEGORY.COMMUNITY],
-  },
-  {
-    name: "Ibrahim Mestadi",
-    message:
-      "Kawtar, the former Chair of the IEEE ENSIAS Student Branch in 2020, is a standout collaborator. We achieved success together on projects like building the branch's website and excelling in the Gitex Dubai IoT & AI challenge under her guidance.\n As a university peer, she's known for her kindness, strong problem-solving skills, and creative flair in front-end development and UI/UX design.\n I confidently endorse her for any future endeavors.",
-    position: "Software Engineer",
-    company: "Inyad",
-    photo: "ibrahim.jpg",
-    date: "September 2023",
-    category: [
-      FEEDBACK_CATEGORY.LEADERSHIP,
-      FEEDBACK_CATEGORY.COMMUNITY,
-      FEEDBACK_CATEGORY.DESIGN,
-      FEEDBACK_CATEGORY.WORK,
-    ],
-  },
+
   {
     name: "Aymane Helfa",
     message:
